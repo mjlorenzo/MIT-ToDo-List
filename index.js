37d2d9b3fb2f4b3ea3f2c4bd0d44080d4241de32
mjlorenzo <mjlorenzo87@gmail.com>
2021-07-28 16:36:00 -0400

    Refactored ToDo Form into form.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // the application will implement all CRUD operations using local state
 
 // define a top level component 
-function App(){
+function App() {
   // define a state variable 'todos', and a function 'setTodos' used to update that variable
   // also define an initial placeholder state for the 'todos' variable
   const [todos, setTodos] = React.useState([
@@ -17,29 +17,20 @@ function App(){
     {
       text: 'build todo app',
       isCompleted: false,
-    }        
+    }
   ])
 
-  // define a state variable 'value' to hold the input from the <input> element and a function 'setValue' to update it
-  const [value, setValue] = React.useState('');
-
-  // define a function 'handleSubmit' to capture the onSubmit event
-  const handleSubmit = e => {
-    // prevent the default behavior of the browser (normally a page reload)
-    e.preventDefault();
-    // check if the value exists, if not return and do nothing
-    if (!value) return;
-
-    // otherwise set the 'todos' state variable with a new array constructed from the old array and the new todo
+  // define a function 'addTodo' to add a todo to the list
+  const addTodo = text => {
     setTodos([...todos, {
-      text: value,
+      text,
       isCompleted: false
     }]);
   }
 
   // define a function 'removeTodo' to remove a todo that has been clicked on
   const removeTodo = e => {
-    // retrieve the key attribute of the clicked on todo
+    // retrieve the id attribute of the clicked on todo
     let index = e.currentTarget.id;
     // if there wasn't one, ignore and return
     if (!index) return;
@@ -52,24 +43,21 @@ function App(){
     setTodos(tempTodos);
   }
   // return our JSX
-  return(
+  return (
     // start with a fragment as a container
     <>
       {/* create a series of <div>'s, one for each todo */}
-      {todos.map((todo, i) => 
+      {todos.map((todo, i) =>
         <div key={i} id={i} className="card" onClick={removeTodo}>
-          <div className ="card-body">{todo.text}</div>
+          <div className="card-body">{todo.text}</div>
         </div>)}
-      {/* create a form with a text input to add new todos */}
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} placeholder="Add ToDo..." onChange={e => setValue(e.target.value)} />
-      </form>
+        <TodoForm addTodo={addTodo} />
     </>
   );
 }
 
 // tell the React DOM to render our top level component
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root')
 );
